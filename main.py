caed50a8e0034efe50eeffb6d670e2d3956d9da8
Fuad Aghazada <fuad.aghazada98@gmail.com>
2019-04-01 19:25:15 +0300

    Blurring and box location is added

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import cv2
 # Initializing Video capture
 cap = cv2.VideoCapture(0)
 
-BOX_X, BOX_Y, BOX_SIZE = 200, 200, 300
-
+BOX_SIZE = 300
+BOX_X, BOX_Y = int(cap.get(3) / 8), int(cap.get(4) / 2 - BOX_SIZE // 2)
 
 while True:
     # Reading frames from Video
@@ -17,10 +17,11 @@ while True:
     # Cropping the image
     cropped_img = gray_scale[BOX_Y:BOX_Y + BOX_SIZE, BOX_X:BOX_X + BOX_SIZE]
 
-    # Applying Adaptive Threshold
-    cropped_img = cv2.adaptiveThreshold(cropped_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 10)
+    # Applying Gaussian Blur
+    cropped_img = cv2.GaussianBlur(cropped_img, (21, 21), 0)
 
-    #
+    # Applying Adaptive Threshold
+    cropped_img = cv2.adaptiveThreshold(cropped_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 
     # Drawing Box in the given coordinates
     cv2.rectangle(frame, (BOX_X, BOX_Y), (BOX_X + BOX_SIZE, BOX_Y + BOX_SIZE), (255, 0, 0), 2)
