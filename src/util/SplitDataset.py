11a1b6391ad8446588944a159531cac949cee053
Emre Sülün <emresulun93@gmail.com>
2019-04-20 15:44:07 +0300

    Samples are organized as train and test

---
+++ src/util/SplitDataset.py
@@ -0,0 +1,39 @@
+"""
+File: SplitDataset
+Author: Emre Sülün
+Date: 20.04.2019
+Project: Sign_Language_Detector
+Description: Splits images into train and test folders. Split ratio is determined by TRAIN_SIZE variable
+"""
+import os
+import shutil
+import string
+
+source = '../samples'
+trainDestination = '../samples/train'
+testDestination = '../samples/test'
+TRAIN_SIZE = 80
+
+chars = {}
+for char in string.ascii_uppercase:
+	chars[char] = TRAIN_SIZE
+
+files = os.listdir(source)
+
+for f in files:
+	letter = f[0]
+	if letter == 't':  # Skip test and train folders
+		continue
+	if chars[letter] > 0:
+		f = source + "/" + f
+		shutil.move(f, trainDestination)
+		chars[letter] = chars[letter] - 1
+
+
+files = os.listdir(source)
+for f in files:
+	letter = f[0]
+	if letter == 't':  # Skip test and train folders
+		continue
+	f = source + "/" + f
+	shutil.move(f, testDestination)
